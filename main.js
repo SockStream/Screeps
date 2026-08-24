7d241a53642c48d0ad474b5cce8603556f952fd9
SockStream <clement.beze@gmail.com>
2026-08-25 00:40:59 +0200

    clean avant le passage en mode "FS"

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
 // main.js : point d'entrée exécuté à chaque tick par Screeps
-// Appelle le ColonyManager pour gérer tous les spawns
-const { cout } = require('./Utils');
-const ColonyManager = require('./ColonyManager');
+// Appelle le ColonyManager pour gérer tous les spawn
 
 module.exports.loop = function () {
-
-    // Code exécuté à chaque tick
-    ColonyManager.run();
-    const spawn = Game.spawns["Spawn1"];
-
-    if (!spawn) {
-        console.log("Spawn1 introuvable");
-        return;
-    }
-
-    const room = spawn.room;
-
-    // Logs utiles pour suivre l'état du monde
-    console.log("Tick :", Game.time);
-    console.log("Nombre de creeps :", Object.keys(Game.creeps).length);
 };
